58223732f78139dc873cd67a9d2881261c76148c
JannatunNimeNishat <nishatnime100@gmail.com>
2023-11-19 19:17:13 +0600

    enum and custom required message added on student model

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -7,56 +7,78 @@ import {
 } from './student.interface';
 
 const userNameSchema = new Schema<UserName>({
-  firstName: { type: String, required: true },
+  firstName: { type: String, required: [true,'first name is required'] },
   middleName: { type: String },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: [true,'last name is required'] },
 });
 
 const guardianSchema = new Schema<Guardian>({
-  fatherName: { type: String, required: true },
-  fatherOccupation: { type: String, required: true },
-  fatherContactNo: { type: String, required: true },
-  motherName: { type: String, required: true },
-  motherOccupation: { type: String, required: true },
-  motherContactNo: { type: String, required: true },
+  fatherName: { type: String, required: [true,'father name is required'] },
+  fatherOccupation: { type: String, required: [true,'fatherOccupation is required'] },
+  fatherContactNo: { type: String, required: [true,'fatherContactNo is required'] },
+  motherName: { type: String, required: [true,'mother name is required'] },
+  motherOccupation: { type: String, required: [true,'motherOccupation is required'] },
+  motherContactNo: { type: String, required: [true,'motherContactNo is required'] },
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-  name: { type: String, required: true },
-  occupation: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  address: { type: String, required: true },
+  name: { type: String, required: [true,'name is required'] },
+  occupation: { type: String, required: [true,'occupation name is required'] },
+  contactNo: { type: String, required: [true,'contactNo is required'] },
+  address: { type: String, required: [true,'address is required'] },
 });
 
 //2. creating student Schema
 const studentSchema = new Schema<Student>({
   id: { type: String },
-  name: userNameSchema,
-  gender: ['male', 'female'], //male or female as par student interface
+  name: {
+    type: userNameSchema,
+    required: [true,'name is required'],
+  },
+  gender: {
+    //male or female as par student interface
+    type: String,
+    enum: ['male', 'female', 'other'],
+    required: [true,'gender is required'],
+  },
   dateOfBirth: { type: String },
-  email: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  emergencyContactNo: { type: String, required: true },
-  bloodGroup: [
-    'A',
-    'B',
-    'AB',
-    'O',
-    'A+',
-    'A-',
-    'B+',
-    'B-',
-    'AB+',
-    'AB-',
-    'O+',
-    'O-',
-  ], //male or female as par student interface
-  presentAddress: { type: String, required: true },
-  permanentAddress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
+  email: { type: String, required: [true,'email is required'] },
+  contactNo: { type: String, required: [true,'contactNo is required'] },
+  emergencyContactNo: { type: String, required: [true,'emergencyContactNo is required'] },
+  bloodGroup: {
+    type: String,
+    enum: [
+      'A',
+      'B',
+      'AB',
+      'O',
+      'A+',
+      'A-',
+      'B+',
+      'B-',
+      'AB+',
+      'AB-',
+      'O+',
+      'O-',
+    ],
+    required: true,
+  },
+  presentAddress: { type: String, required: [true,'presentAddress is required'] },
+  permanentAddress: { type: String, required: [true,'permanentAddress is required'] },
+  guardian: {
+    type: guardianSchema,
+    required: [true,'guardian is required'],
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: [true,'localGuardian is required'],
+  },
   profileImg: { type: String },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 });
 
 // 3. Create a Model.
