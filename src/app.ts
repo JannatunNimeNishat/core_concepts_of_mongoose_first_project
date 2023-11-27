79fc95c9ddcb21c33251a1b244094d75bae3f671
JannatunNimeNishat <nishatnime100@gmail.com>
2023-11-27 14:37:22 +0600

    global not found middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorhandler from './app/middlwares/globalErrorhandler';
 import router from './app/routes';
+import notFound from './app/middlwares/notFound';
 const app: Application = express();
 
 //parser
@@ -15,16 +15,16 @@ app.use('/api/v1', router);
 /* app.use('/api/v1/students', studentRoutes);
 app.use('/api/v1/users', UserRouters); */
 
-
-
 //test route
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 });
 
-
 //global error handling middleware
 app.use(globalErrorhandler);
+
+//not found route
+app.use(notFound);
 
 export default app;
