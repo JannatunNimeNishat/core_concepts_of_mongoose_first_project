005d67fdc7d45eb169a6640a64bd9b726e258e7b
JannatunNimeNishat <nishatnime100@gmail.com>
2023-11-28 20:44:14 +0600

    catchAsync hider oder function added on the utlis folder to avoid using try catch on every controller

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,62 +1,53 @@
-import {RequestHandler} from 'express';// RequestHandler -> createStudent er req:Request, res:Response next:NextFunction er type auto declare kore dey
+
 import { StudentServices } from './student.service';
 import sendResponse from '../../utils/sendResponse';
 
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const getStudents:RequestHandler = async (req, res, next) => {
-  try {
-    //service
-    const result = await StudentServices.getAllStudentsFromDB();
-    
-    sendResponse(res,{
-      success:true,
-      statusCode: httpStatus.OK,
-      message:'students are retrieved successfully',
-      data:result
-    })
-  } catch (error) {
-    
-    next(error); //sending the error to global error handler on app.ts
-  }
-};
 
-const getSingleStudent:RequestHandler = async (req, res, next) => {
-  try {
+
+const getStudents = catchAsync(async (req, res) => {
+  const result = await StudentServices.getAllStudentsFromDB();
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'students are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleStudent = catchAsync(async (req, res) => {
+
     //
     const { studentId } = req.params;
     //calling service
     const result = await StudentServices.getSingleStudentFormDB(studentId);
-   
-    sendResponse(res,{
-      success:true,
-      statusCode: httpStatus.OK,
-      message:'student is retrieved successfully',
-      data:result
-    })
-  } catch (error) {
-    
-    next(error)
-  }
-};
 
-const deleteStudent:RequestHandler = async (req, res, next) => {
-  try {
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'student is retrieved successfully',
+      data: result,
+    });
+  
+});
+
+const deleteStudent= catchAsync(async (req, res) => {
+  
     const { studentId } = req.params;
 
     const result = await StudentServices.deleteStudentFromDB(studentId);
-  
-    sendResponse(res,{
-      success:true,
+
+    sendResponse(res, {
+      success: true,
       statusCode: httpStatus.OK,
-      message:'Successfully deleted',
-      data:result
-    })
-  } catch (error) {
-    
-    next(error)
-  }
-};
+      message: 'Successfully deleted',
+      data: result,
+    });
+ 
+});
 
 export const StudentControllers = {
   getStudents,
---
+++ src/app/utils/catchAsync.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express'; // RequestHandler -> createStudent er req:Request, res:Response next:NextFunction er type auto declare kore dey
+
+//avoid the try catch my using higher order function catchAsync
+const catchAsync = (fn: RequestHandler) => {
+    return (res: Response, req: Request, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next)).catch((err) => next(err)); // if the promise is not resolved we are sending the errors to our global error handler function
+    };
+  };
+
+  export default catchAsync;
