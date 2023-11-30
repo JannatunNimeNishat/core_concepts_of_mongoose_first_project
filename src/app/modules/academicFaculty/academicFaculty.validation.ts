ed3cc9af467f7b51a4144cbffd0a3d279454713c
JannatunNimeNishat <nishatnime100@gmail.com>
2023-11-30 20:08:19 +0600

    academicFaculty validation with zod added

---
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+/* const academicFacultyValidationSchema = z.object({
+    body:z.object({
+        name:z.string({
+            invalid_type_error:'Academic faculty must be a string'
+        })
+    })
+}) */
+const academicFacultyValidationSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic faculty must be a string',
+  }),
+});
+
+export const academicFacultyValidations = {
+  academicFacultyValidationSchema,
+};
