6cb05e205c7b2c6ba48db0afeef12c83107232a1
JannatunNimeNishat <nishatnime100@gmail.com>
2023-11-30 20:15:39 +0600

    academicFaculty service added

---
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,37 @@
+import { TAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFaculty } from './academicFaculty.model';
+
+const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
+  const result = await AcademicFaculty.create(payload);
+
+  return result;
+};
+
+const getAllAcademicFacultyFromDB = async () => {
+  const result = await AcademicFaculty.find();
+  return result;
+};
+
+const getSingleAcademicFacultyFromDB = async (payload: string) => {
+  const result = await AcademicFaculty.findOne({ _id: payload });
+  return result;
+};
+
+const updateAcademicFacultyIntoDB = async (
+  semesterId: string,
+  payload: Partial<TAcademicFaculty>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate(
+    { _id: semesterId },
+    payload,
+    { new: true },
+  );
+  return result;
+};
+
+export const AcademicSemesterServices = {
+  createAcademicFacultyIntoDB,
+  getAllAcademicFacultyFromDB,
+  getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
+};
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -3,30 +3,7 @@ import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
-  /*  //validating the semester code.
-amra check korbo 
-// Semester Code
-//Autumn 01
-//Summer 02
-//Fall 03
-je data asteca saitar modde je Semester name dice saita ar oi semester name er jonno amra je code tik kore dici saita match kore ki na. like Autumn er jonno 01 ase ki na.
-*/
-
- /* type TAcademicSemesterNameCodeMapper = {
-    // Autumn: '01';
-   // Summer: '02';
-   // Fall: '03'; 
-    //or we can use map type
-
-    [key:string]:string;
-
-  };*/
-
- /*  const academicSemesterNameCodeMapper:TAcademicSemesterNameCodeMapper = {
-    Autumn: '01',
-    Summer: '02',
-    Fall: '03',
-  }; */
+ 
 
   if(academicSemesterNameCodeMapper[payload.name] !== payload.code){ 
     //1.academicSemesterNameCodeMapper[Autumn] = '01'
