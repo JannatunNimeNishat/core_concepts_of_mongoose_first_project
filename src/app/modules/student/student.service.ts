dcdec531a774c37d83aac91e62dfe324fd00c627
JannatunNimeNishat <nishatnime100@gmail.com>
2023-12-02 00:33:42 +0600

    transaction and rollback added on delete user and student

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,5 +1,9 @@
 // import { TStudent } from './student.interface';
+import { startSession } from 'mongoose';
 import { Student } from './student.model';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import { User } from '../user/user.model';
 
 const getAllStudentsFromDB = async () => {
   // const result = await Student.find().populate('admissionSemester').populate('academicDepartment');
@@ -28,21 +32,37 @@ const getSingleStudentFormDB = async (id: string) => {
   return result;
 };
 
+//transaction and rollback delete 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne(
-    {
-      id: id,
-    },
-    {
-      isDeleted: true,
-    },
-    /*  {
-      $set: {
-        isDeleted: true,
-      },
-    }, */
-  );
-  return result;
+  const session =await startSession();
+  try {
+    session.startTransaction();
+
+    //->deleting student (transaction-1)
+
+    //amra amader created kora id deya delete korte casci ti findOneAndUpdate use korte hobe. mongoose er _id deya korte cile amra findById korte partam
+    const deletedStudent = await Student.findOneAndUpdate({id: id, },{isDeleted: true},{new: true, session});
+
+    if(!deletedStudent){
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
+    }
+
+    //-> deleting user (transaction-2)
+    const deletedUser = await User.findOneAndUpdate({id:id},{isDeleted:true},{new:true, session})
+    if(!deletedUser){
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deletedStudent;
+
+
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+  }
 };
 
 export const StudentServices = {
