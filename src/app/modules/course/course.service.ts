48a0ed9c827f2be3c3b5933c2aa184288d9aceec
JannatunNimeNishat <nishatnime100@gmail.com>
2023-12-08 11:48:31 +0600

    createCourse, getAllCourse, getSingleCourse, deleteCourse methods are added on course.service.ts file

---
+++ src/app/modules/course/course.service.ts
@@ -0,0 +1,27 @@
+import { Course } from './course.model';
+
+const createCourseIntoDB = async () => {
+  const result = await Course.create();
+  return result;
+};
+
+const getAllCoursesFromDB = async () => {
+  const result = await Course.find();
+  return result;
+};
+
+const getSingleCourseFromDB = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
+const deleteCourseFromDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(id,{isDeleted:true},{new:true});
+  return result;
+};
+
+export const courseServices = {
+  createCourseIntoDB,
+  getAllCoursesFromDB,
+  getSingleCourseFromDB,
+  deleteCourseFromDB
+};
