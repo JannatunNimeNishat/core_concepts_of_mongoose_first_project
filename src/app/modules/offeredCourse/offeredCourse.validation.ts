0e89cde4cdf8fe01dc86ae9016086e024fa39728
JannatunNimeNishat <nishatnime100@gmail.com>
2023-12-17 18:02:56 +0600

    time validation added with .refine and regex also end > start at offerCourse validation

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -12,8 +12,29 @@ const createOfferedCourseValidationSchema = z.object({
     maxCapacity: z.number(),
     section: z.number(),
     days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string(),
-    endTime: z.string(),
+     // amra time ta Hours:Minutes ai pattern a chai. ei pattern ta amra .refine method er modde regex user kore fixed kore dite pari. 
+    startTime: z.string().refine((time)=>{
+        const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+        return regex.test(time);
+    },{
+        message:'Invalid time format, expected "HH:MM" in 24 hours format'
+    }),
+    endTime: z.string().refine((time)=>{
+        const regex =  /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+        return regex.test(time);
+    },{
+        message:'Invalid time format, expected "HH:MM" in 24 hours format'
+    }),
+  }).refine((body)=>{
+    //amra casci startTime ar endTime jate akta r aktar pore hoy. 12:30 jate kokhon o 10:30 er age na cole ase. 
+    //startTime: 10:30 => 1970-01-01T10:30 //endTime: 12:30 => 1970-01-01T12:30
+    //amader date ta aki rakte hobe. comparison ta acan e time er sate hobe. r 1970-01-01 teka utc date calu hoyce so standard dora hoyce. 
+
+    const start = new Date(`1970-01-01T${body?.startTime}:00`);
+    const end = new Date(`1970-01-01T${body?.endTime}:00`);
+    return end > start; // end time start time er teka boro hote hobe. 
+  },{
+    message:'Stat time should be before end time!.'
   }),
 });
 const updateOfferedCourseValidationSchema = z.object({
