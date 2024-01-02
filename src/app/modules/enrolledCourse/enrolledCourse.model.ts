e00aee2852cf9e4d0e8f63c94698c6418f4c8eef
JannatunNimeNishat <nishatnime100@gmail.com>
2024-01-02 22:39:55 +0600

    Updated enrolledCourse model

--- src/app/modules/enrolledCourse/enrolledCourse.model.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.model.ts
@@ -22,30 +22,35 @@ const enrolledCourseSchema = new Schema<TEnrolledCourse>({
     type: Schema.ObjectId,
     required: true,
     ref: 'SemesterRegistration',
+    trim:true
   },
   academicSemester: {
     type: Schema.ObjectId,
     required: true,
     ref: 'AcademicSemester',
+    trim:true
   },
   academicFaculty: {
     type: Schema.ObjectId,
     required: true,
     ref: 'AcademicFaculty',
+    trim:true
   },
   academicDepartment: {
     type: Schema.ObjectId,
     required: true,
     ref: 'AcademicDepartment',
+    trim:true
   },
   offeredCourse: {
     type: Schema.ObjectId,
     required: true,
     ref: 'OfferedCourse',
+    trim:true
   },
-  course: { type: Schema.ObjectId, required: true, ref: 'Course' },
-  student: { type: Schema.ObjectId, required: true, ref: 'Student' },
-  faculty: { type: Schema.ObjectId, required: true, ref: 'Faculty' },
+  course: { type: Schema.ObjectId, required: true, ref: 'Course',trim:true },
+  student: { type: Schema.ObjectId, required: true, ref: 'Student',trim:true },
+  faculty: { type: Schema.ObjectId, required: true, ref: 'Faculty',trim:true },
   isEnrolled: { type: Boolean, required: true, default: false },
   courseMarks: { type: courseMarksSchema, default: {} },
   grade: { type: String, enum: Grade, default: 'NA' },
